29de548b773d2d0bf88dbac9779ea67796b14470
Cole Taylor <coletaylor788@gmail.com>
2017-09-22 14:46:14 -0400

    Changing debug host to runtime host

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def generate_schedule():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
 
     #TESTING ONLY! Leave commented in production
-    app.run(host='127.0.0.1', port=port)
+    #app.run(host='127.0.0.1', port=port)

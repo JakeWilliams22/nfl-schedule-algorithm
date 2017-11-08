8e19bafce14c45ce9f1715c6c8c35758f934aef4
Cole Taylor <coletaylor788@gmail.com>
2017-11-08 14:13:50 -0500

    Adding optional schedule to optimizer

--- src/schedule_optimizer.py
+++ src/schedule_optimizer.py
@@ -134,9 +134,9 @@ my_schedule_take_step = ScheduleTakeStep()
 initial_schedule = generate_random_schedule(game_days, nfl_teams)
 
 # Note: this function minimizes the cost (1 - score) of the schedule using the Basin Hopping algorithm
-def optimize_schedule():
+def optimize_schedule(init_sched = initial_schedule):
     optimizer_result = optimize.basinhopping(score_schedule_cost, \
-                                             initial_schedule, \
+                                             init_sched, \
                                              minimizer_kwargs=dict(method=noop_min), \
                                              take_step=my_schedule_take_step)
     sched = optimizer_result.x.item(0)
